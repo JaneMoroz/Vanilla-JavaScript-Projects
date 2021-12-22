ee1764f8f409ccf4a28e13becca85089b5841863
Jane Moroz <Moroz.jane@yahoo.com>
2021-12-22 11:56:20 +0300

    New Year Countdown

---
+++ New Year Countdown/script.js
@@ -0,0 +1,44 @@
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+const countdown = document.getElementById('countdown');
+const year = document.getElementById('year');
+const loading = document.getElementById('loading');
+
+const currentYear = new Date().getFullYear();
+
+const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+
+// Set background year
+year.innerText = currentYear + 1;
+
+// Update countdown time
+function updateCountdown() {
+  const currentTime = new Date();
+  const diff = newYearTime - currentTime; // in mileseconds
+
+  // Calculate days
+  const d = Math.floor(diff / 1000 / 60 / 60 / 24); // dif / 1000 mileseconds/ 60 seconds/ 60 minutes / 24 hours
+  // Calculate hours
+  const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+  // Calculate minutes
+  const m = Math.floor(diff / 1000 / 60) % 60;
+  // Calculate seconds
+  const s = Math.floor(diff / 1000) % 60;
+
+  // Add values to DOM
+  days.innerHTML = d;
+  hours.innerHTML = h < 10 ? "0" + h : h;
+  minutes.innerHTML = m < 10 ? "0" + m : m;
+  seconds.innerHTML = s < 10 ? "0" + s : s;
+}
+
+// Show spinner before countdown
+setTimeout(() => {
+  loading.remove();
+  countdown.style.display = 'flex';
+}, 1000);
+
+// Run every second
+setInterval(updateCountdown, 1000);
